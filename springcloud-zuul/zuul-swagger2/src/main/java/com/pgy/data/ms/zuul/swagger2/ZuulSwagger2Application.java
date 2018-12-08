2adb0432f96aeaf09ecea1e55e431644fa4d536f
ginko <1244791244@qq.com>
2018-12-09 03:14:37 +0800

    init

--- springcloud-zuul/zuul-swagger2/src/main/java/com/pgy/data/ms/zuul/swagger2/ZuulSwagger2Application.java
+++ springcloud-zuul/zuul-swagger2/src/main/java/com/pgy/data/ms/zuul/swagger2/ZuulSwagger2Application.java
@@ -1,9 +1,19 @@
 package com.pgy.data.ms.zuul.swagger2;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+/**
+ * zuul网关
+ * @author ginko
+ */
 @SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+@Slf4j
 public class ZuulSwagger2Application {
 
     public static void main(String[] args) {
