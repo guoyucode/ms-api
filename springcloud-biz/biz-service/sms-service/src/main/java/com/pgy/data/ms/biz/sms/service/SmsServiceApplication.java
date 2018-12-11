bde1fed8675e33fed5c8d832c579150f156360db
ginko <1244791244@qq.com>
2018-12-11 12:47:12 +0800

    init

--- springcloud-biz/biz-service/sms-service/src/main/java/com/pgy/data/ms/biz/sms/service/SmsServiceApplication.java
+++ springcloud-biz/biz-service/sms-service/src/main/java/com/pgy/data/ms/biz/sms/service/SmsServiceApplication.java
@@ -1,12 +1,17 @@
 package com.pgy.data.ms.biz.sms.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
+@Slf4j
 public class SmsServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SmsServiceApplication.class, args);
+        log.info("SmsServiceApplication 短信服务启动...");
     }
 }
